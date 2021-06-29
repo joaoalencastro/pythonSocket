23277aa3d4dbeb06c8e16b69c69a2352950f485c
joaoalencastro <joao.alencastro@gmail.com>
2021-06-29 15:21:04 -0300

    Feat: all the main functionalities working

--- client.py
+++ client.py
@@ -1,6 +1,23 @@
+#!/usr/bin/env python3
+
 import socket
+import hashlib
+from datetime import datetime
+
+HOST = '127.0.0.1'
+PORT = 8080
 
 # create an INET, STREAMing socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# now connect to the web server on port 80 - the normal http port
-s.connect(("localhost", 8080))
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    # now connect to the web server on port 80 - the normal http port
+    s.connect((HOST, PORT))
+    # get the current timestamp
+    time = datetime.now()
+    # make the hash string
+    result = hashlib.md5(str(time).encode())
+    # send the result
+    s.sendall(result.digest())
+    # now wait for the response and save it to data
+    data = s.recv(1024)
+
+print('Received', repr(data))
--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import socket
 
 '''
@@ -5,17 +7,27 @@ import socket
 ### https://docs.python.org/3/howto/sockets.html
 '''
 
-# create an INET, STREAMing socket
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# bind the socket to a public host, and a well-known port
-serversocket.bind((socket.gethostname(), 8080))
-# become a server socket
-serversocket.listen(5)
+HOST = '127.0.0.1'
+PORT = 8080
 
-while True:
-    # accept connections from outside
-    (clientsocket, address) = serversocket.accept()
-    # now do something with the clientsocket
-    # in this case, we'll pretend this is a threaded server
-    ct = client_thread(clientsocket)
-    ct.run()
+# create an INET, STREAMing socket | Using with statement means we don't have to close the socket
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serversocket:
+    # bind the socket to a public host, and a well-known port
+    serversocket.bind((HOST, PORT))
+    # become a server socket
+    serversocket.listen(5)
+
+    while True:
+        # accept connections from outside
+        (conn, addr) = serversocket.accept()
+        # now do something with the clientsocket
+        with conn:
+            print('Connected by', addr)
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall(data)
+        # in this case, we'll pretend this is a threaded server
+        #ct = client_thread(clientsocket)
+        #ct.run()
