ba5e9072c03cbf1a9f26a75556a989d075d5fa40
joaoalencastro <joao.alencastro@gmail.com>
2021-06-29 10:58:37 -0300

    modified server.py

--- server.py
+++ server.py
@@ -1,15 +1,14 @@
 import socket
 
-addr = ("", 8080)  # all interfaces, port 8080
-if socket.has_dualstack_ipv6():
-    s = socket.create_server(addr, family=socket.AF_INET6, dualstack_ipv6=True)
-else:
-    s = socket.create_server(addr)
+'''
+### Este código está sendo baseado na seguinte página web:
+### https://docs.python.org/3/howto/sockets.html
+'''
 
 # create an INET, STREAMing socket
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # bind the socket to a public host, and a well-known port
-serversocket.bind((socket.gethostname(), 80))
+serversocket.bind((socket.gethostname(), 8080))
 # become a server socket
 serversocket.listen(5)
 
@@ -20,9 +19,3 @@ while True:
     # in this case, we'll pretend this is a threaded server
     ct = client_thread(clientsocket)
     ct.run()
-
-
-
-
-### Este código está sendo baseado na seguinte página web:
-### https://docs.python.org/3/howto/sockets.html
